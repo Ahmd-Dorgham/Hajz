77ee36ab9051780849b3d71640f88072b8b7e4fe
Ahmed Dorgham <ahmeddorgham779@gmail.com>
2024-12-17 00:36:42 +0200

    Add reservation model

---
+++ DB/Models/reservation.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const reservationSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    tableId: {
+      type: Schema.Types.ObjectId,
+      ref: "Table",
+      required: true,
+    },
+    mealId: {
+      type: Schema.Types.ObjectId,
+      ref: "Meal",
+      required: false, // Optional mealId
+    },
+    restaurantId: {
+      type: Schema.Types.ObjectId,
+      ref: "Restaurant",
+      required: true,
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["reserved", "canceled", "completed"],
+      default: "reserved",
+    },
+  },
+  { timestamps: true }
+);
+
+const Reservation = model("Reservation", reservationSchema);
+export default mongoose.models.Reservation || Reservation;
